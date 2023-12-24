845136dac16bbedf2ea899e626bdb465b2a78774
Jesse Roberts <jesse@jesseops.net>
2023-12-24 13:27:45 -0700

    yeet those extra errors

--- internal/pkg/routes/errors.go
+++ internal/pkg/routes/errors.go
@@ -3,59 +3,33 @@ package routes
 import (
 	"github.com/jesseops/coppermind/internal/app/services"
 	"github.com/labstack/echo/v4"
-	echomw "github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 func errRoutes(c *services.Container, g *echo.Group) {
-	g.Use(echomw.Logger())
-	g.GET("/500", func(ctx echo.Context) error {
-		return ctx.String(http.StatusInternalServerError, "500")
-	})
-	g.GET("/404", func(ctx echo.Context) error {
-		return ctx.String(http.StatusNotFound, "404")
-	})
-	g.GET("/403", func(ctx echo.Context) error {
-		return ctx.String(http.StatusForbidden, "403")
+	g.GET("/400", func(ctx echo.Context) error {
+		return ctx.String(http.StatusBadRequest, "400")
 	})
 	g.GET("/401", func(ctx echo.Context) error {
 		return ctx.String(http.StatusUnauthorized, "401")
 	})
-	g.GET("/400", func(ctx echo.Context) error {
-		return ctx.String(http.StatusBadRequest, "400")
+	g.GET("/403", func(ctx echo.Context) error {
+		return ctx.String(http.StatusForbidden, "403")
+	})
+	g.GET("/404", func(ctx echo.Context) error {
+		return ctx.String(http.StatusNotFound, "404")
+	})
+	g.GET("/418", func(ctx echo.Context) error {
+		return ctx.String(http.StatusTeapot, "418")
 	})
 	g.GET("/429", func(ctx echo.Context) error {
 		return ctx.String(http.StatusTooManyRequests, "429")
 	})
+	g.GET("/500", func(ctx echo.Context) error {
+		return ctx.String(http.StatusInternalServerError, "500")
+	})
 	g.GET("/503", func(ctx echo.Context) error {
 		return ctx.String(http.StatusServiceUnavailable, "503")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
-	})
-	g.GET("/418", func(ctx echo.Context) error {
-		return ctx.String(http.StatusTeapot, "418")
 	})
 	g.GET("/panic", func(ctx echo.Context) error {
 		panic("test")
--- internal/pkg/routes/router.go
+++ internal/pkg/routes/router.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
-    "github.com/jesseops/coppermind/internal/app/services"
+	"github.com/jesseops/coppermind/internal/app/services"
+	echomw "github.com/labstack/echo/v4/middleware"
 )
 
 func SetupRoutes(c *services.Container) {
-    group := c.WebServer.Group("")
+	group := c.WebServer.Group("")
+	group.Use(echomw.Logger())
 
-    // Error routes
-    errRoutes(c, group)
+	// Error routes
+	errRoutes(c, group)
 }
-
